eba3c4250ae3ca88dd86119528ad28ccf8037200
Sid Ahmed Boudaoud <js_boudaoud@esi.dz>
2026-07-11 00:58:15 +0100

    frontend is good to go

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://back-academy.onrender.com/api',
 });
 
 // Automatically attach the token to every request
